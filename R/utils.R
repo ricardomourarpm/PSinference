429d32330bc3c2cd9332bee4c33f53008995f22c
Danial Mazarei <71553400+dmazarei@users.noreply.github.com>
2023-06-25 23:16:24 +0330

    add stop

--- R/utils.R
+++ R/utils.R
@@ -2,9 +2,13 @@ partition <- function(array, nrows, ncols) {
   #split matrix into submatrices
   r <- dim(array)[1]
   c <- dim(array)[2]
+  if(r>nrows & c>ncols){
+    stop("ncols or nrows is larger than array .")
+  }
   A <- array[1:nrows, 1:ncols]
   B <- array[1:nrows, (ncols + 1):c]
   C <- array[(nrows + 1):r, 1:ncols]
   D <- array[(nrows + 1):r, (ncols + 1):c]
   return(list(A, B, C, D))
 }
+
