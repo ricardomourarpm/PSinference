fa5b5531db842d75bdd862a1b9219d1c4467b70e
Danial Mazarei <71553400+dmazarei@users.noreply.github.com>
2023-06-26 02:42:08 +0330

    change function

--- R/GVdist.R
+++ R/GVdist.R
@@ -1,10 +1,26 @@
+#' GVdist
+#'
+#' This function calculates
+#'
+#' @param nsample .
+#' @param pvariates .
+#' @param iterations .
+#'
+#' @references
+#'  ref
+#'
+#' @examples
+#' GVdist(nsample = 10, pvariates = 4, iterations = 5)
+#'
+#' @export
+
+
 GVdist <- function(nsample, pvariates, iterations) {
-  T <- c()
+  T <- rep(NA, iterations)
   for (i in 1:iterations) {
     q <- chiprod(pvariates, nsample - 1)
     p <- chiprod(pvariates, nsample - 1)
-    T <- c(T, p * q)
+    T[i] <- p * q
   }
   return(T)
-
 }
