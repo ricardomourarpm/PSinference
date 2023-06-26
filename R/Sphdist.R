d7f433cf1ce450db9157d81a86b8f8f0d8144ad4
Danial Mazarei <71553400+dmazarei@users.noreply.github.com>
2023-06-27 01:37:54 +0330

    change function

--- R/Sphdist.R
+++ R/Sphdist.R
@@ -10,16 +10,16 @@
 #'  ref
 #'
 #' @examples
-#' sphdist(nsample = 100, pvariates = 4, iterations = 2)
+#' sphdist(nsample = 100, pvariates = 4, iterations = 4)
 #'
 #' @export
 
-Sphdist <- function(nsample, pvariates, iterations) {
+sphdist <- function(nsample=100, pvariates=4, iterations=2) {
   T <- rep(NA, iterations)
   W1 <- rWishart(iterations, nsample - 1, diag(pvariates) / (nsample - 1))
   W2 <- rWishart(iterations, nsample - 1, diag(pvariates))
   for (i in 1:iterations) {
-    T[i] <- det(W1[,,i] %*% W2[,,i])^(1/pvariates) / sum(W1 %*% W2)
+    T[i] <- det(W1[,,i] %*% W2[,,i])^(1/pvariates) / sum(W1[,,i] %*% W2[,,i])
   }
   return(T)
 }
