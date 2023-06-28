b62eabd0766322e25fa8da674c001fc1f1643a9c
Danial Mazarei <71553400+dmazarei@users.noreply.github.com>
2023-06-29 00:44:26 +0330

    remove

--- R/Inddist.R
+++ R/Inddist.R
@@ -14,22 +14,24 @@
 #' Inddist(part = 2, nsample = 100, pvariates = 4, iterations = 2)
 #'
 #' @export
-Inddist(p1, n, p, sim)
-part <- p1
-nsample <- n
-pvariates <- p
-iterations <- sim
+
 Inddist <- function(part, nsample, pvariates, iterations) {
   T <- rep(NA, iterations)
   W1 <- stats::rWishart(iterations, nsample - 1, diag(pvariates))
-  for (i in 1:iterations) {
-    W2 <- stats::rWishart(1, nsample - 1, W1[, , i] / (nsample - 1))
-    A <- partition(W2[, , 1], part, part)
-    W2_11 <- A[[1]]
-    W2_22 <- A[[4]]
-    if (p == 1) {
+  if (part == 1) {
+    for (i in 1:iterations) {
+      W2 <- stats::rWishart(1, nsample - 1, W1[, , i] / (nsample - 1))
+      A <- partition(W2[, , 1], part, part)
+      W2_11 <- A[[1]]
+      W2_22 <- A[[4]]
       T[i] <- det(W2[, , 1]) / (W2_11 * det(W2_22))
-    } else {
+    }
+  } else {
+    for (i in 1:iterations) {
+      W2 <- stats::rWishart(1, nsample - 1, W1[, , i] / (nsample - 1))
+      A <- partition(W2[, , 1], part, part)
+      W2_11 <- A[[1]]
+      W2_22 <- A[[4]]
       T[i] <- det(W2[, , 1]) / (det(W2_11) * det(W2_22))
     }
   }
