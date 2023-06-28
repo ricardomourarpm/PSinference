8c9381f93a7fb7073cf0830b2c801ccab084c840
Danial Mazarei <71553400+dmazarei@users.noreply.github.com>
2023-06-29 00:42:20 +0330

    change for

--- R/Inddist.R
+++ R/Inddist.R
@@ -15,10 +15,10 @@
 #'
 #' @export
 Inddist(p1, n, p, sim)
-part=p1
-nsample=n
-pvariates=p
-iterations=sim
+part <- p1
+nsample <- n
+pvariates <- p
+iterations <- sim
 Inddist <- function(part, nsample, pvariates, iterations) {
   T <- rep(NA, iterations)
   W1 <- stats::rWishart(iterations, nsample - 1, diag(pvariates))
@@ -27,7 +27,11 @@ Inddist <- function(part, nsample, pvariates, iterations) {
     A <- partition(W2[, , 1], part, part)
     W2_11 <- A[[1]]
     W2_22 <- A[[4]]
-    T[i] <- det(W2[, , 1]) / (W2_11*det(W2_22))
+    if (p == 1) {
+      T[i] <- det(W2[, , 1]) / (W2_11 * det(W2_22))
+    } else {
+      T[i] <- det(W2[, , 1]) / (det(W2_11) * det(W2_22))
+    }
   }
   return(T)
 }
