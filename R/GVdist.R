ddda10ac33c3a1471127f3acca853b32fceed35c
mnrzrad <mina.norouzirad@gmail.com>
2023-07-04 18:30:45 +0100

    error

--- R/GVdist.R
+++ R/GVdist.R
@@ -1,15 +1,19 @@
-#' GVdist
+#' GVdist  is
 #'
-#' This function calculates the near-exact distribution of the pivotal random variable
+#' This function calculates the empirical distribution of the pivotal random variable that can be used to perform inferential procedures
+#' for the Generalized Variance based on the released Single Synthetic data generated under Plug-in Sampling.
 #' \deqn{T_1^\star = (n-1)\frac{|S^*|}{|\Sigma|}}
 #' where \eqn{S^\star = \sum_{i=1}^n (v_i - \bar{v})(v_i - \bar{v})^{\top}} and \eqn{v_i} is the \eqn{i}th observation of the synthetic dataset.
 #' Its distribution is
 #' \deqn{T_1^* \sim  \prod_{i=1}^n \chi_{n-i}^2 \prod_{i=1}^p \chi_{n-i}^2}
 #' where \eqn{\chi_{n-i}^2} are all independent chi-square random variables.
-#'
 #' The \eqn{(1-\alpha)} level confidence interval for \eqn{|\Sigma|} is given by
 #' \deqn{\left(\frac{(n-1)^p|\tilde{S}^\star|}{t^\star_{1,1-\alpha/2}},\frac{(n-1)^p|\tilde{S}^\star|}{t^\star_{1,\alpha/2}} \right)}
+<<<<<<< HEAD
 #' where \eqn{|\tilde{S}^\star|} is the observed value of \eqn{|\{S}^\star|} and \eqn{t^\star_{1,\gamma}} is the \eqn{\gamma}th percentile of \eqn{T_1}.
+=======
+#' where \eqn{\tilde{S}^\star} is the observed value of \eqn{S^\star} and \eqn{t^\star_{1,\gamma}} is the \eqn{\gamma}th percentile of \eqn{T_1}.
+>>>>>>> df9ecaabe94b6ee5ca4168d6fd366cbe0cdbf6cd
 #'
 #' @param nsample Sample size.
 #' @param pvariates Number of variables.
@@ -19,11 +23,19 @@
 #'  Klein, M., Moura, R. and Sinha, B. (2021) Multivariate Normal Inference based on Singly Imputed Synthetic Data under Plug-in Sampling. Sankhya B 83, 273–287.
 #'
 #' @examples
+<<<<<<< HEAD
 #' library(matrixStats)
 #' library(ggplot2)
 #' # Original data created
 #' # two different population variances (determinants)
 #' Sigma1 <- matrix(c(1, 0.5, 0.5, 0.5,
+=======
+#' library(MASS)
+#'
+#'# Original data created
+#'# two different population variances (determinants)
+#'Sigma1 <- matrix(c(1, 0.5, 0.5, 0.5,
+>>>>>>> df9ecaabe94b6ee5ca4168d6fd366cbe0cdbf6cd
 #'                   0.5, 1, 0.5, 0.5,
 #'                   0.5, 0.5, 1, 0.5,
 #'                   0.5, 0.5, 0.5, 1), nrow = 4, ncol = 4, byrow = TRUE)
@@ -32,9 +44,16 @@
 #'                   0.5, 2, 0, 0,
 #'                   0, 0, 3, 0.2,
 #'                   0, 0, 0.2, 4), nrow = 4, ncol = 4, byrow = TRUE)
+<<<<<<< HEAD
 #'
 #'df_o1 = simOrigData(100, 4, mu = c(1,0,5,0), Sigma = Sigma1)
 #'df_o2 = simOrigData(100, 4, mu = c(1,0,5,0), Sigma = Sigma2)
+=======
+#' n_sample = 100
+#' # Create original simulated dataset
+#' df_o1 = mvrnorm(n_sample, mu = c(1,0,5,0), Sigma = Sigma1)
+#' df_o2 = mvrnorm(n_sample, mu = c(1,0,5,0), Sigma = Sigma2)
+>>>>>>> df9ecaabe94b6ee5ca4168d6fd366cbe0cdbf6cd
 #'
 #'# Sim data created
 #'
@@ -42,6 +61,7 @@
 #'df_s2 = simSynthData(df_o2)
 #'
 #'
+<<<<<<< HEAD
 #' # Gather the 0.025 and 0.975 quantiles and construct confident interval for sigma^2
 #' # Check that sigma^2 is inside in both cases
 #' p1 = dim(df_s1)[2]
@@ -57,6 +77,23 @@
 #' cat(left,right,'\n')
 #' print(det(Sigma1))
 #' # The synthetic value is inside the confidence interval of GV
+=======
+#'# Gather the 0.025 and 0.975 quantiles and construct confident interval for sigma^2
+#'# Check that sigma^2 is inside in both cases
+#'p1 = dim(df_s1)[2]
+#'p2 = dim(df_s2)[2]
+#'
+#'T1 <- GVdist(100, p1, 10000)
+#'q975 <- quantile(T1, 0.975)
+#'q025 <- quantile(T1, 0.025)
+#'
+#'left <- (100-1)^p1*det(cov(df_s1)*99)/q975
+#'right <- (100-1)^p1*det(cov(df_s1)*99)/q025
+#'
+#'cat(left,right,'\n')
+#'print(det(Sigma1))
+#'# The synthetic value is inside the confidence interval of GV
+>>>>>>> df9ecaabe94b6ee5ca4168d6fd366cbe0cdbf6cd
 #'
 #' @export
 
