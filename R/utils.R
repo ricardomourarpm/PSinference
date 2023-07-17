c49eee52390875dd91a8c4f6393b12317ffa5592
Ricardo Moura <ricardo.moura.rpm@gmail.com>
2023-07-17 14:48:19 +0100

    deleted partition

--- R/utils.R
+++ R/utils.R
@@ -1,18 +1,3 @@
-partition <- function(mat, nrows, ncols) {
-  # split matrix into submatrices
-  r <- dim(mat)[1]
-  c <- dim(mat)[2]
-  if (r < nrows & c < ncols) {
-    stop("ncols or nrows is larger than array .")
-  }
-  A <- mat[1:nrows, 1:ncols]
-  B <- mat[1:nrows, (ncols + 1):c]
-  C <- mat[(nrows + 1):r, 1:ncols]
-  D <- mat[(nrows + 1):r, (ncols + 1):c]
-  return(list(A, B, C, D))
-}
-
-
 chiprod <- function(dimension, degrees, iterations = 1) {
   A <- matrix(stats::rchisq(dimension * iterations, (degrees - dimension + 1):degrees),
     nrow = dimension, ncol = iterations
