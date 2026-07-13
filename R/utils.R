e6ab2f4f2a4d32cfc6309a74d1d2ae3f6771c4dc
VitorCanhaoAugusto <vitorcanhao96@gmail.com>
2026-07-13 14:07:39 +0100

    bug fix

--- R/utils.R
+++
@@ -1,7 +0,0 @@
-chiprod <- function(dimension, degrees, iterations = 1) {
-  A <- matrix(stats::rchisq(dimension * iterations, (degrees - dimension + 1):degrees),
-    nrow = dimension, ncol = iterations
-  )
-  product <- apply(A, 2, prod)
-  return(product)
-}
